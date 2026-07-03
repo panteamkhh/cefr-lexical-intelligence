fe8f655ef647305ce029f7f6795dd4f4eb81db3f
Panteamkhh <pantea.mkh18@gmail.com>
2026-07-03 11:59:33 +0330

    add 03 -notebook

--- src/check.py
+++
@@ -1,7 +0,0 @@
-import chardet
-
-
-file_path = r"D:\list of words\cefr-project\data\raw\toefl_vocabulary_cleaned_categorized.xlsx"
-
-with open(file_path, 'rb') as f:
-    print(chardet.detect(f.read(100000)))
